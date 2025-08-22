8c572dba59a720e49c209910700c11b1f96e4d92
axel456-ADM <axelcono0505@gmail.com>
2025-08-21 19:30:48 -0600

    Update exercises_01_177499.py

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -6,26 +6,30 @@
 # comienza cada nombre con mayúscuala y el resto con minúsculas incluye acentos y otros
 # caracteres del Español.
 # Escribe el código aquí
-
+nombre = "De Ávila Martínez Axel"
 
 
 # 1.1 Declara una variable matricula y asigna tu número de matrícula como entero
 # Escribe el código aquí
-
+matricula = 177499
 
 
 # 2. Crear una función que sume dos números
 def sumar(a, b):
-    pass # reemplaza pass con tu código
+    return a + b # reemplaza pass con tu código
+sumar(1,2)
 
 
 # 3. Usar un bucle for para imprimir los números del 1 al 10
 # reemplaza la palabra reservada pass con tu código
 def bucle_for():
-    pass
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for i in nums:
+        print(i)
 
 
 # 5. Usar un bucle para imprimir cada número
 def print_numbers():
     numbers = [1, 2, 3, 4, 5]
-    # Escribe el código aquí
+    for number in numbers:
+      print(number)
